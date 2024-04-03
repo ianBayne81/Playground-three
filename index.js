b017dfabbb72a719ea70746d1b579e4cf410e9b5
Ian Bayne <43926592+ianBayne81@users.noreply.github.com>
2024-04-03 16:34:50 +1000

    Improved functions
    Re did Javascript functions to make the game play in the correct format

--- index.js
+++ index.js
@@ -1,137 +1,148 @@
-
-//create game constructor function
-class Game {
-    constructor() {
-    this.remainingGames = 3
-    this.cpuResult = []
-    this.playerResult = []
-    this.cpuScore = 0
-    this.playerScore = 0
-    this.status = "Playing"
-    }
-}
-
 //key variables
-const newGame = new Game
-const summaryEl = document.querySelector("#summary")
-const cpuEl = document.querySelector("#cpuScore")
-const playerEl = document.querySelector("#playerScore")
 const startButton = document.querySelector("#startButton")
 const rockEl = document.querySelector("#rock")
 const paperEl = document.querySelector("#paper")
 const scissorsEl = document.querySelector("#scissors")
 const playerChoice = document.querySelector(".player-symbol")
 const cpuChoice= document.querySelector(".cpu-symbol")
+const restartEl= document.querySelector("#restart")
+let newGame
 
+//create gsap animation 
+const animate = gsap.timeline({repeat:-1}).timeScale(1)
+animate.fromTo("#nuckle", {y: 0}, {y: -10, ease:'powerOne'}).pause()
 
-//cpu's choices
-const cpuArray = ["Rock", "Paper", "Scissors"]
-let result =  cpuArray[Math.floor(Math.random() * cpuArray.length)]
-
-const cpuResult = function () {
-
-    if (result == "Rock") {
-        cpuChoice.classList.toggle("cpu-rock")
-    } else if (result == "Paper") {
-        cpuChoice.classList.toggle("cpu-paper")
-    } else if (result == "Scissors") {
-        cpuChoice.classList.toggle("cpu-scissors")
+//create game constructor function
+class Game {
+    constructor() {
+    this.cpuArray = ["Rock", "Paper", "Scissors"]
+    this.cpuResult = ""
+    this.cpuScore = 0
+    this.playerScore = 0
+    this.status = "Playing"
     }
-}
 
-//renderScores 
-const renderScores = function () {
-    cpuEl.innerHTML = ""
-    const createCpuScore = document.createElement("p")
-    createCpuScore.textContent = newGame.cpuScore
-    cpuEl.appendChild(createCpuScore)
-
-    playerEl.innerHTML = ""
-    const createPlayerScore = document.createElement("p")
-    createPlayerScore.textContent = newGame.playerScore
-    playerEl.appendChild(createPlayerScore)
-}
-
-//render status summary
-const renderStatus = function () {
+    renderStatus() {
+    const summaryEl = document.querySelector("#summary")
     summaryEl.innerHTML = ""
     let summary = document.createElement("p")
-    summary.textContent = newGame.status
+    summary.textContent = this.status
     summaryEl.appendChild(summary)
-}
+    }
 
+    
+    renderScores() {
+    const cpuEl = document.querySelector("#cpuScore")
+    cpuEl.innerHTML = ""
+    const createCpuScore = document.createElement("p")
+    createCpuScore.textContent = this.cpuScore
+    cpuEl.appendChild(createCpuScore)
 
-//render newGame info to the header element
-const render = function() {  
+    const playerEl = document.querySelector("#playerScore")
+    playerEl.innerHTML = ""
+    const createPlayerScore = document.createElement("p")
+    createPlayerScore.textContent = this.playerScore
+    playerEl.appendChild(createPlayerScore)
+    }
 
-    //call render status function
-    renderStatus()
+    clear() {
+        playerChoice.className = "player-symbol"
+        cpuChoice.className = "cpu-symbol"
+        this.status = "Playing"
+    }
 
-    //eventlisteners for selection
-    rockEl.addEventListener("click", (e) => {
-        let classResult = e.target.className  
-        playerChoice.classList.toggle(classResult)
-        cpuResult()
-        if (result === "Rock") {
-            newGame.status = "It's a tie!"
-        } else if (result ==="Paper") {
-            newGame.status = "CPU wins!"
-            newGame.cpuScore = newGame.cpuScore + 1
-        } else if (result === "Scissors") {
-            newGame.status = "Player wins!"
-            newGame.playerScore = newGame.playerScore + 1
-        }
+    cpuFunction() {
+        this.cpuResult = this.cpuArray[Math.floor(Math.random() * this.cpuArray.length)]
         
-        renderStatus()
-        renderScores()
-    })
-
-    paperEl.addEventListener("click", (e) => {
-        let classResult = e.target.className  
-        playerChoice.classList.toggle(classResult)
-        cpuResult()
-        if (result === "Rock") {
-            newGame.status = "Player wins!"
-            newGame.playerScore = newGame.playerScore + 1
-        } else if (result ==="Paper") {
-            newGame.status = "It's a tie!"
-        } else if (result === "Scissors") {
-            newGame.status = "CPU wins!"
-            newGame.cpuScore = newGame.cpuScore + 1
-            
+        if (this.cpuResult == "Rock") {
+            cpuChoice.classList.toggle("cpu-rock")
+        } else if (this.cpuResult == "Paper") {
+            cpuChoice.classList.toggle("cpu-paper")
+        } else if (this.cpuResult == "Scissors") {
+            cpuChoice.classList.toggle("cpu-scissors")
         }
-
-        renderStatus()
-        renderScores()
-
-    })
-
-    scissorsEl.addEventListener("click", (e) => {
-        let classResult = e.target.className  
-        playerChoice.classList.toggle(classResult)
-        cpuResult()
-        if (result === "Rock") {
-            newGame.status = "CPU wins!"
-            newGame.cpuScore = newGame.cpuScore + 1
-        } else if (result ==="Paper") {
-            newGame.status = "Player wins!"
-            newGame.playerScore = newGame.playerScore + 1
-        } else if (result === "Scissors") {
-            newGame.status = "It's a tie!"
-        }
-
-        renderStatus()
-        renderScores()
-
-    })
-
+    }
+    
 }
 
 
 //start new game when click button
-startButton.addEventListener("click", function (e) {
+startButton.addEventListener("click", (e) => {
     e.preventDefault()
-    newGame
-    render()
-    renderScores()
+    newGame = new Game()
+    newGame.clear()
+    newGame.renderStatus()
+    newGame.renderScores()
+    animate.resume()
+
 })
+
+restartEl.addEventListener("click", (e) => {
+    e.preventDefault()
+    animate.resume()
+    newGame.clear()
+    newGame.renderStatus()
+})
+
+rockEl.addEventListener("click", (e) => {
+    let classResult = e.target.className  
+    playerChoice.classList.toggle(classResult)
+    newGame.cpuFunction()
+    if (newGame.cpuResult === "Rock") {
+        newGame.status = "It's a tie!"
+    } else if (newGame.cpuResult ==="Paper") {
+        newGame.status = "CPU wins!"
+        newGame.cpuScore = newGame.cpuScore + 1
+    } else if (newGame.cpuResult === "Scissors") {
+        newGame.status = "Player wins!"
+        newGame.playerScore = newGame.playerScore + 1
+    }
+    
+    animate.pause()
+    newGame.renderStatus()
+    newGame.renderScores()
+
+})
+        
+paperEl.addEventListener("click", (e) => {
+    let classResult = e.target.className  
+    playerChoice.classList.toggle(classResult)
+    newGame.cpuFunction()
+    if (newGame.cpuResult === "Rock") {
+        newGame.status = "Player wins!"
+        newGame.playerScore = newGame.playerScore + 1
+    } else if (newGame.cpuResult ==="Paper") {
+        newGame.status = "It's a tie!"
+    } else if (newGame.cpuResult === "Scissors") {
+        newGame.status = "CPU wins!"
+        newGame.cpuScore = newGame.cpuScore + 1
+                
+    }
+    
+    animate.pause()
+    newGame.renderStatus()
+    newGame.renderScores()
+
+})
+
+scissorsEl.addEventListener("click", (e) => {
+    let classResult = e.target.className  
+    playerChoice.classList.toggle(classResult)
+    newGame.cpuFunction()
+    if (newGame.cpuResult === "Rock") {
+        newGame.status = "CPU wins!"
+        newGame.cpuScore = newGame.cpuScore + 1
+    } else if (newGame.cpuResult ==="Paper") {
+        newGame.status = "Player wins!"
+        newGame.playerScore = newGame.playerScore + 1
+    } else if (newGame.cpuResult === "Scissors") {
+        newGame.status = "It's a tie!"
+    }
+
+    animate.pause()
+    newGame.renderStatus()
+    newGame.renderScores()
+
+})
+
+    
